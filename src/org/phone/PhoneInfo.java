30573b23da6e4c8b0287448e1509574ed1486196
prawin1714 <prawin.v75@gmail.com>
2022-04-21 19:26:02 +0530

    PK's Code

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -1,40 +1,11 @@
 package org.phone;
 
 public class PhoneInfo {
-
-	private void phoneName() {
-		System.out.println("Phone Number - 99999999");
-
-	}
-	
-	private void phoneMieiNum() {
-		System.out.println("Phone Miei Mumber - xxxxxxx");
-
-	}
-	
-	private void Camera() {
-		System.out.println("Camera - 88pxl");
-
-	}
-	private void storage() {
-		System.out.println("Storage - xxx");
-
-	}
-	private void osName() {
-		System.out.println("OS Name - Andriod");
-
-	}
 	
 	public static void main(String[] args) {
 		
-		PhoneInfo p=new PhoneInfo();
-		
-		p.phoneName();
-		p.phoneMieiNum();
-		p.Camera();
-		p.storage();
-		p.osName();
-		
+		System.out.println("3");
+		System.out.println("4");
 	}
 	
 }
